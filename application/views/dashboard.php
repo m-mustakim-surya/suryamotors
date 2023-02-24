8382937a604da8a6f5a4d434f67024a8311f2762
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-02-24 11:45:04 +0700

    get data

---
+++ application/views/dashboard.php
@@ -0,0 +1,16 @@
+<div class="container-fluid">
+    <div class="row text-center">
+        <?php foreach($barangs as $barang) : ?>
+            <div class="card ml-4" style="width: 18rem;">
+                <img src="<?php echo base_url().'/uploads/'.$barang->gambar ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title mb-1"><?php echo $barang->nama_barang ?></h5>
+                    <small><?php echo $barang->keterangan ?></small><br>
+                    <span class="badge badge-pill badge-success mb-3">Rp. <?php echo $barang->harga ?></span><br>
+                    <a href="#" class="btn btn-sm btn-dark">Tambah ke Chart</a>
+                    <a href="#" class="btn btn-sm btn-secondary">Detail</a>
+                </div>
+            </div>
+        <?php endforeach?>
+    </div>
+</div>
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -4,7 +4,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
@@ -67,13 +67,6 @@
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
 
-            <!-- Sidebar Message -->
-            <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-            </div>
-
         </ul>
         <!-- End of Sidebar -->
 
@@ -98,7 +91,7 @@
                             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
                                 aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
+                                <button class="btn btn-dark" type="button">
                                     <i class="fas fa-search fa-sm"></i>
                                 </button>
                             </div>
@@ -123,7 +116,7 @@
                                             placeholder="Search for..." aria-label="Search"
                                             aria-describedby="basic-addon2">
                                         <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
+                                            <button class="btn btn-dark" type="button">
                                                 <i class="fas fa-search fa-sm"></i>
                                             </button>
                                         </div>
