ca11394600d4e5887bd2ec5147f0144f6a57f9de
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-02-27 14:35:37 +0700

    keranjang & detail keranjang

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,9 +6,10 @@
                 <div class="card-body">
                     <h5 class="card-title mb-1"><?php echo $barang->nama_barang ?></h5>
                     <small><?php echo $barang->keterangan ?></small><br>
-                    <span class="badge badge-pill badge-success mb-3">Rp. <?php echo $barang->harga ?></span><br>
-                    <a href="#" class="btn btn-sm btn-dark">Tambah ke Chart</a>
+                    <span class="badge badge-pill badge-success mb-3">Rp. <?php echo number_format($barang->harga, 0, ',','.') ?></span><br>
+                    <?php echo anchor('dashboard/tambah_ke_keranjang/' . $barang->id_barang, '<div class="btn btn-sm btn-dark">Tambah ke Keranjang</div>') ?>
                     <a href="#" class="btn btn-sm btn-secondary">Detail</a>
+
                 </div>
             </div>
         <?php endforeach?>
